e0791a18bb040031574be1eae4c2c8796cbf72e6
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-07-28 19:43:37 +0200

    Automatically print Public Key for Github to console after creating it

--- CreateGithubSSH-Key.sh
+++ CreateGithubSSH-Key.sh
@@ -7,4 +7,10 @@ else
 echo "The configured SSH Key doesn't exist. Let's create one."
 read -p "Your Email: " email
     ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
+    echo
+    cat ~/.ssh/id_rsa_github.pub
+    echo
+    echo "Add This Key to your Github Account before you continue."
+    echo
+    history -c
 fi
