2a323906a5367629777b848fa65d685de1c041bc
Raphael Kuttruf <Raphael.Kuttruf@vollert.de>
2020-09-08 18:13:42 +0200

    Add feature use user.email as SSH Key email.

--- CreateGithubSSHKey.sh
+++ CreateGithubSSHKey.sh
@@ -2,10 +2,17 @@
 
 if [[ -f ~/.ssh/id_rsa_github ]]
 then
-echo "Your SSH Key id_rsa_github for Github already exists."
+    echo "Your SSH Key id_rsa_github for Github already exists."
 else
-echo "Your SSH Key id_rsa_github for Github doesn't exist. Let's create it."
-read -p "Your Email: " email
+    echo "Your SSH Key id_rsa_github for Github doesn't exist. Let's create it."
+
+    read -p "Use Git user.email [y/n]: " email
+    if [[ $email == "y" ]] 
+    then
+        email=$(git config --global --get user.email)
+    else
+        read -p "Your Email: " email
+    fi
     ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
     echo
     cat ~/.ssh/id_rsa_github.pub
