ba3e16da1a4247e2139ca85e9446017271ec12ee
Raphael Kuttruf <Raphael.Kuttruf@vollert.de>
2020-09-08 17:39:46 +0200

    More robust Key creation

--- CreateGitSigningKey.sh
+++ CreateGitSigningKey.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-name=$(git config --global --get user.name)
-email=$(git config --global --get user.email)
 signingkey=$(git config --global --get user.signingkey)
 
-if [[  ${#signingkey} == 16 ]] # signingkey == 16 characters
+if [[ ! -z $signingkey ]] && [[ ${#signingkey} == 16 ]] # signingkey is not empty and exactly 16 characters long
 then
 echo "Your Git user.signingkey is: "$signingkey
 else
 echo "No global user.signingkey configured for Git. Let's create it."
 echo 
 echo "Use RSA, Size 4096"
-    gpg --full-generate-key
+    if  [[ -z $(gpg --list-secret-keys) ]] # no keys exist
+    then
+        gpg --full-generate-key
+    fi
     gpg --list-secret-keys --keyid-format LONG
-    read -p "Key (sec)[SC]: " signingkey
+    read -p "Key |sec| rsa4096/*key* [SC]: " signingkey
     git config --global user.signingkey "$signingkey"
     gpg --armor --export $signingkey
     echo "Add the public key above to your Github Accout."
+    ./CreateGitSigningKey.sh
     history -c
 fi
