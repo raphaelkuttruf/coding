731988403526483a68f8f02064723d34d02a165d
Raphael Kuttruf <Raphael.Kuttruf@gmx.net>
2020-09-21 12:27:15 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# secure acess to ssh directory and files
-touch ~/.ssh
+# Create ssh directory
+if [[ -d ~/.ssh ]]
+then
+    echo "Your SSH directory exists."
+else
+    mkdir ~/.ssh
+fi
+
+# Secure acess to ssh directory and files
 chmod 700 ~/.ssh
 touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
@@ -31,6 +38,12 @@ else
 fi
 
 ssh -T git@github.com
+
+# Install git-lfs package if it is not installed
+#if [ $(dpkg-query -W -f='${Status}' git-lfs 2>/dev/null | grep -c "ok installed") -eq 0 ] 
+#then
+#sudo apt install git-lfs
+#fi
 
 git lfs install
 
