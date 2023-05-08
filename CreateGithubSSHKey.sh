f94a2d25cc6148e6cc62bcb5ddf2c5dcef2d3039
Raphael Kuttruf <Raphael.Kuttruf@vollert.de>
2023-05-08 12:54:32 +0200

    Update SSH Key Creation

--- CreateGithubSSHKey.sh
+++ CreateGithubSSHKey.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ -f ~/.ssh/id_rsa_github ]]
+if [[ -f ~/.ssh/id_github ]]
 then
-    echo "Your SSH Key id_rsa_github for Github already exists."
+    echo "Your SSH Key id_github for Github already exists."
 else
-    echo "Your SSH Key id_rsa_github for Github doesn't exist. Let's create it."
+    echo "Your SSH Key id_github for Github doesn't exist. Let's create it."
 
     read -p "Use Git user.email [y/n]: " email
     if [[ $email == "y" ]] 
@@ -13,9 +13,9 @@ else
     else
         read -p "Your Email: " email
     fi
-    ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
+    ssh-keygen -t ed25519 -C "$email" -f ~/.ssh/id_github
     echo
-    cat ~/.ssh/id_rsa_github.pub
+    cat ~/.ssh/id_github.pub
     echo
     echo "Add this Key to your Github Account before you continue."
     echo
